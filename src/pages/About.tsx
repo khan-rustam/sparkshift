74347db474d37a232f4ca8c80c3c8ed36efde919
Rustam Khan <khanrustam2809@gmail.com>
2024-12-14 20:16:54 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -22,7 +22,7 @@ const team = [
   },
   {
     image:
-      "https://southcentralus1-mediap.svc.ms/transform/thumbnail?provider=spo&farmid=194091&inputFormat=jpg&cs=MDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDQ4MTcxMGE0fFNQTw&docid=https%3A%2F%2Fmy.microsoftpersonalcontent.com%2F_api%2Fv2.0%2Fdrives%2Fb!JU2-SjlE80yq9YWEOhJAGguqkXQS_tpOs0i184C5N2ktUbc-xmibQ4tlV1Kt9Yuv%2Fitems%2F0175MCW62FMJD7Y64FWBCI2FPJ6I7L5E2W%3Ftempauth%3Dv1e.eyJzaXRlaWQiOiI0YWJlNGQyNS00NDM5LTRjZjMtYWFmNS04NTg0M2ExMjQwMWEiLCJhcHBpZCI6IjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDA0ODE3MTBhNCIsImF1ZCI6IjAwMDAwMDAzLTAwMDAtMGZmMS1jZTAwLTAwMDAwMDAwMDAwMC9teS5taWNyb3NvZnRwZXJzb25hbGNvbnRlbnQuY29tQDkxODgwNDBkLTZjNjctNGM1Yi1iMTEyLTM2YTMwNGI2NmRhZCIsImV4cCI6IjE3MzMzMTM2MDAifQ.jgEVtByJnn0AbVYGeJxMD-abmTOPtQ8g2ww6MAYoRZcCzUbDKH_JfXU5WUoQP0zbj-CW8Qt3rTnQHHNri5H86FeH5_C3QVbDAjqP4-O7XsxWLGDH8rF_Tm5h9z1-lM80kdbBN_c-BouamPKzuMFFX5ZVgMZ4IFhJBm9T11ZenmBXGXiS0yORVLTTAoP7xwBZGV32IxTd2JtzXgE1SbjuHhIJvK1k3aVB8O5VxsOqi8OsWRrFvSnOYYmD_3Wa69IOCGCoB132RlFgyDC6vytI_87rfPyXaSRCKKI5E8YsDI15KJUPUs8Bo1B-BcKkKfK5hrUMVTD51I91ITK58qtDNpWYdhm-PeQ69WPRNDbEfvpvrD3C5WBOgYXge8F2qhJb.i5Elobv-6ZDC2SWBhZ8rAMl9LbypmJa8eowQdo2iOEg%26version%3DPublished&cb=63868897693&encodeFailures=1&width=649&height=1187&action=Access",
+      "https://media.licdn.com/dms/image/v2/D5603AQGskB3_FihE1w/profile-displayphoto-shrink_400_400/B56ZPFoeQyHoAg-/0/1734187542529?e=1739404800&v=beta&t=bpLAnPIMciWRPGT84689c7365VUsD7jAvHpNSrtI4ro",
     name: "Rustam",
     role: "Development Director",
   },
